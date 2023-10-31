3d0fd8ae78a0caa115df0de475c4a5c98ec79de8
noemiBa <banalnoemi@gmail.com>
2023-10-31 15:01:24 +0000

    Restructured code to encapsulate spec downloading and loading within GitHubAPIHandler

--- ai_assistant.py
+++ ai_assistant.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
@@ -14,27 +15,40 @@ class GPTNeoAssistant:
         return response
 
 class GitHubAPIHandler:
-    def __init__(self):
-        pass  
+    def __init__(self, spec_url, spec_filename):
+        self.spec_filename = spec_filename
+        if not os.path.exists(spec_filename):
+            self.download_api_spec(spec_url, spec_filename)
+        self.load_spec(spec_filename)
     
-    def handle_query(self, query): #checks the query for certain keywords and returns hardcoded responses.
+    def download_api_spec(self, url, filename):
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+    
+    def load_spec(self, filename):
+        with open(filename, 'r') as file:
+            self.api_spec = json.load(file)
+    
+    def handle_query(self, query):
         if "authenticate" in query:
             return "To authenticate your requests, use the 'Authorization' header with your personal access token."
 
-gpt_neo_assistant = GPTNeoAssistant()
-github_api_handler = GitHubAPIHandler()
-
-
-
-def api_call(endpoint, headers):
-    response = requests.get(endpoint, headers=headers)
-    return json.loads(response.text)
-
-
 def handle_query(query):
-    pass
+    if "GitHub API" in query:
+        response = github_api_handler.handle_query(query)
+    else:
+        response = gpt_neo_assistant.generate_response(query)
+    print(response)
+
+api_spec_url = 'https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json'
+api_spec_filename = 'api_spec.json'
+
+gpt_neo_assistant = GPTNeoAssistant()
+github_api_handler = GitHubAPIHandler(api_spec_url, api_spec_filename)
 
 if __name__ == "__main__":
     while True:
-        query = input("Ask me a question about GitHub API: ")
+        query = input("Ask me a question: ")
         handle_query(query)
