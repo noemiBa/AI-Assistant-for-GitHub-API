a6f605a2cb1a453edfed0f42bad369c35423e51e
noemiBa <banalnoemi@gmail.com>
2023-10-31 16:03:02 +0000

    Add method to GPTNeoAssistant to generate responses based on API info

--- ai_assistant.py
+++ ai_assistant.py
@@ -13,6 +13,13 @@ class GPTNeoAssistant:
         outputs = self.model.generate(inputs, max_length=150, temperature=0.7, pad_token_id=self.tokenizer.eos_token_id)
         response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return response
+    
+    def generate_api_response(self, info):
+        if info:
+            response_text = f"Path: {info['path']}\nMethod: {info['method']}\nSummary: {info['summary']}\nDescription: {info['description']}"
+            return response_text
+        else:
+            return "I couldn't find relevant information for your query."
 
 class GitHubAPIHandler:
     def __init__(self, spec_url, spec_filename):
@@ -27,35 +34,46 @@ class GitHubAPIHandler:
         with open(filename, 'wb') as file:
             file.write(response.content)
 
-    def print_spec_summary(self):
-        print(f"Keys at the root level: {list(self.api_spec.keys())}")
-        print(f"Keys under 'paths': {list(self.api_spec['paths'].keys())[:5]}")  # Print first 5 keys under 'paths'
-        print(f"Details of one endpoint: {self.api_spec['paths']['/repos/{owner}/{repo}']}")
+    # def print_spec_summary(self):
+    #     print(f"Keys at the root level: {list(self.api_spec.keys())}")
+    #     print(f"Keys under 'paths': {list(self.api_spec['paths'].keys())[:5]}")  # Print first 5 keys under 'paths'
+    #     print(f"Details of one endpoint: {self.api_spec['paths']['/repos/{owner}/{repo}']}")
+    
+    def preprocess_query(self, query):  
+        keywords = query.lower().split()
+        return keywords
+
+    def lookup_info(self, keywords):
+        for path, operations in github_api_handler.api_spec['paths'].items():
+            for method, operation in operations.items():
+                if any(keyword in operation['description'].lower() for keyword in keywords):
+                    return {
+                        'path': path,
+                        'method': method.upper(),
+                        'summary': operation.get('summary', ''),
+                        'description': operation.get('description', ''),
+                        'parameters': operation.get('parameters', []),
+                        'responses': operation.get('responses', {})
+                    }
+        return None
     
     def load_spec(self, filename):
         with open(filename, 'r', encoding='utf-8') as file:
             self.api_spec = json.load(file)
-    
-    def handle_query(self, query):
-        if "authenticate" in query:
-            return "To authenticate your requests, use the 'Authorization' header with your personal access token."
 
 def handle_query(query):
-    if "GitHub API" in query:
-        response = github_api_handler.handle_query(query)
-    else:
-        # response = gpt_neo_assistant.generate_response(query)
-        response = "GPT-Neo is temporarily disabled."
-    print(response)
+        keywords = github_api_handler.preprocess_query(query)  
+        info = github_api_handler.lookup_info(keywords) 
+        response = gpt_neo_assistant.generate_api_response(info)  
+        print(response)
 
 api_spec_url = 'https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json'
 api_spec_filename = 'api_spec.json'
 
-#gpt_neo_assistant = GPTNeoAssistant()
+gpt_neo_assistant = GPTNeoAssistant()
 github_api_handler = GitHubAPIHandler(api_spec_url, api_spec_filename)
 
 if __name__ == "__main__":
     while True:
         query = input("Ask me a question: ")
-        github_api_handler.print_spec_summary()
         handle_query(query)
